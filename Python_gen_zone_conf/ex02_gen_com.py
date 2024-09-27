128605c7ca431b2301de935d3e77823332d5d404
Shigehiro Hattori <narihiro.h@gmail.com>
2024-09-27 18:16:55 +0900

    minor refactoring

--- Python_gen_zone_conf/ex02_gen_com.py
+++ Python_gen_zone_conf/ex02_gen_com.py
@@ -2,26 +2,76 @@
 
 zone_prefix = 'example'
 zone_suffix = 'com'
-number_of_zone = 100
+number_of_zone = 100000
 
+# Generate zone file
 with open('com.zone.file.txt', 'w') as f:
+    conf_string = """
+$TTL 3600
+@ IN SOA ns01 ns01 (
+2010062303
+1h
+15m
+30d
+1h )
+  IN NS ns01.com.
+  IN NS ns02.com.
+
+ns01 IN A 192.168.55.42
+ns01 IN AAAA 2001:db8:1::42
+ns02 IN A 192.168.55.43
+ns02 IN AAAA 2001:db8:1::43
+"""
+    f.write(conf_string)
+
+# Generate zone file
+with open('com.zone.file.txt', 'a') as f:
     for i in range(0,number_of_zone):
+        domain_number = str(f"{i:06}")
+
         name_conf_string = f"""
-{zone_prefix}{i}.{zone_suffix}. IN NS ns01.{zone_prefix}{i}.{zone_suffix}.
-{zone_prefix}{i}.{zone_suffix}. IN NS ns02.{zone_prefix}{i}.{zone_suffix}.
-{zone_prefix}{i}.{zone_suffix}. IN NS ns03.{zone_prefix}{i}.{zone_suffix}.
-{zone_prefix}{i}.{zone_suffix}. IN NS ns04.{zone_prefix}{i}.{zone_suffix}.
-ns01.{zone_prefix}{i}.{zone_suffix}. IN A 192.168.103.44
-ns02.{zone_prefix}{i}.{zone_suffix}. IN A 192.168.103.45
-ns03.{zone_prefix}{i}.{zone_suffix}. IN A 192.168.103.46
-ns04.{zone_prefix}{i}.{zone_suffix}. IN A 192.168.193.47
+{zone_prefix}{domain_number}.{zone_suffix}. IN NS ns01.{zone_prefix}{domain_number}.{zone_suffix}.
+{zone_prefix}{domain_number}.{zone_suffix}. IN NS ns02.{zone_prefix}{domain_number}.{zone_suffix}.
+{zone_prefix}{domain_number}.{zone_suffix}. IN NS ns03.{zone_prefix}{domain_number}.{zone_suffix}.
+{zone_prefix}{domain_number}.{zone_suffix}. IN NS ns04.{zone_prefix}{domain_number}.{zone_suffix}.
+ns01.{zone_prefix}{domain_number}.{zone_suffix}. IN A 192.168.55.44
+ns01.{zone_prefix}{domain_number}.{zone_suffix}. IN AAAA 2001:db8:1::44
+ns02.{zone_prefix}{domain_number}.{zone_suffix}. IN A 192.168.55.45
+ns02.{zone_prefix}{domain_number}.{zone_suffix}. IN AAAA 2001:db8:1::45
+ns03.{zone_prefix}{domain_number}.{zone_suffix}. IN A 192.168.55.46
+ns03.{zone_prefix}{domain_number}.{zone_suffix}. IN AAAA 2001:db8:1::46
+ns04.{zone_prefix}{domain_number}.{zone_suffix}. IN A 192.168.55.47
+ns04.{zone_prefix}{domain_number}.{zone_suffix}. IN AAAA 2001:db8:1::47
 """
         f.write(name_conf_string)
 
+# Generate named.conf
 with open('example.com_named.conf.txt', 'w') as f:
+    conf_string = """
+options {
+  directory "/var/cache/bind";
+  dnssec-validation false;
+  allow-query { any; };
+  recursion no;
+  allow-recursion { none; };
+  listen-on { any; };
+  listen-on-v6 { any; };
+  notify no;
+};
+
+zone  "." in {
+        type hint;
+        file "/var/lib/bind/named.ca";
+        };
+"""
+    f.write(conf_string)
+
+# Generate named.conf
+with open('example.com_named.conf.txt', 'a') as f:
     for i in range(0,number_of_zone):
+        domain_number = str(f"{i:06}")
         name_conf_string = f"""
-zone "{zone_prefix}{i}.{zone_suffix}" in {{
+zone "{zone_prefix}{domain_number}.{zone_suffix}" in {{
   type master;
   file "/var/lib/bind/example.com.template.db";
 }};
@@ -29,7 +79,8 @@ zone "{zone_prefix}{i}.{zone_suffix}" in {{
 """
         f.write(name_conf_string)
 
-print("run the following")
-print("$ cat com.db.orig com.zone.file.txt > ../bind_config_ex02/com/records/com.db")
-print("$ cat example.com.named.conf.orig example.com_named.conf.txt > ../bind_config_ex02/example.com/config/named.conf")
+
+print("Copy the file and restart the containers")
+print("$ cp com.zone.file.txt ../bind_config_ex02/com/records/com.db")
+print("$ cp example.com_named.conf.txt ../bind_config_ex02/example.com/config/named.conf")
 print("$ docker compose restart")
